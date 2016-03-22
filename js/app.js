6b436ff5ba64c73c03fc09db46bb4774ec6af2de
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-03-22 07:08:28 -0700

    Game is set up with correct player turns, validation so a player can't mark where there already is a mark, and it displays the current user's turn. Still need to add in the solution definition to determine when someone wins.

--- js/app.js
+++ js/app.js
@@ -2,5 +2,39 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  clickCounter = 0;
+  $("#playerTurn").text("X");
+
+
+  $(".box").on("click",function() {
+      if($(this).text()) {
+          clickCounter += 1;
+          alert("Cant do that");
+
+      }
+      else {
+          if (clickCounter % 2 === 0) {
+              $(this).text("X");
+              $(this).addClass("X clicked");
+              clickCounter += 1;
+              $("#playerTurn").text("O");
+          }
+          else if (clickCounter % 2 !== 0) {
+              $(this).text("O");
+              $(this).addClass("O clicked");
+              clickCounter += 1;
+              $("#playerTurn").text("X");
+          }
+      }
+  });
+
+  $(".reset").on("click", function() {
+      $(".box").text("");
+  });
+
+  if (($(".box-1-1").text() === "X") && ($(".box-1-2").text() === "X") && ($(".box1-3").text() === "X")) {
+      alert("You won!");
+  }
+
 
 });
